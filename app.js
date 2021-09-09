8eb307e3875a245e30c152050bbe1f9f9e9cf833
Oleksandr Shershak <scherschak@gmail.com>
2021-09-09 16:53:29 +0300

    Add music/sounds, change buttons style. Minor change in logic

--- app.js
+++ app.js
@@ -3,19 +3,26 @@ document.addEventListener('DOMContentLoaded', () => {
     let squares = Array.from(document.querySelectorAll('.grid div'))
     const scoreDisplay = document.querySelector('#score')
     const startBtn = document.querySelector('#start-button')
+    const musicBtn = document.querySelector('#music-button')
     const width = 10
     let nextRandom = 0
     let timerId
     let score = 0
     const colors = [
-        'orange',
-        'red',
-        'purple',
-        'green',
-        'blue'
+        '#43aa8b',
+        '#fb5607',
+        '#ff006e',
+        '#8338ec',
+        '#3a86ff'
       ]
+    const sounds = {
+        'fall' : 1,
+        'clear': 2
+    }
+    let isMusicPlay = true
 
-    //The Tetrominoes
+
+    // The Tetrominoes
     const lTetromino = [
         [1, width+1, width*2+1, 2],
         [width, width+1, width+2, width*2+2],
@@ -56,20 +63,21 @@ document.addEventListener('DOMContentLoaded', () => {
     let currentPosition = 4
     let currentRotation = 0
 
-    //randomly select a Tetromino and its first rotation
+    // randomly select a Tetromino and its first rotation
     let random = Math.floor(Math.random()*theTetrominoes.length)
     let current = theTetrominoes[random][currentRotation]
 
 
-    //draw the Tetromino
+    // draw the Tetromino
     function draw() {
         current.forEach(index => {
             squares[currentPosition + index].classList.add('tetromino')
             squares[currentPosition + index].style.backgroundColor = colors[random]
+            squares[currentPosition + index].style.opacity = '0.9'
         })
         }
     
-    //undraw the Tetromino
+    // undraw the Tetromino
     function undraw() {
         current.forEach(index => {
             squares[currentPosition + index].classList.remove('tetromino')
@@ -77,33 +85,35 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
 
-    //assing functions to keyCodes
+    // assing functions to keyCodes
     function control(e) {
-        if(e.keyCode === 37) {
-          moveLeft()
-        } else if (e.keyCode === 38) {
-          rotate()
-        } else if (e.keyCode === 39) {
-          moveRight()
-        } else if (e.keyCode === 40) {
-          moveDown()
+        if(timerId) {
+            if(e.keyCode === 37) {
+                moveLeft()
+            } else if (e.keyCode === 38) {
+                rotate()
+            } else if (e.keyCode === 39) {
+                moveRight()
+            } else if (e.keyCode === 40) {
+                moveDown()
+            }
         }
-      }
+    }
     document.addEventListener('keyup', control)
 
-    //move down function
+    // move down function
     function moveDown() {
-        undraw()
-        currentPosition += width
-        draw()
-        freeze()
+            undraw()
+            currentPosition += width
+            draw()
+            freeze()
     }
 
-    //freeze functions
+    // freeze functions
     function freeze() {
         if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
             current.forEach(index => squares[currentPosition + index].classList.add('taken'))
-            
+            playSound(sounds.fall)
             //start new tetrimino falling
             random = nextRandom
             nextRandom = Math.floor(Math.random()*theTetrominoes.length)
@@ -112,10 +122,11 @@ document.addEventListener('DOMContentLoaded', () => {
             draw()
             displayShape()
             addScore()
+            gameOver()
         }
     }
 
-    //move the tetromino left, unless is at the edge or there is a blockage
+    // move the tetromino left, unless is at the edge or there is a blockage
     function moveLeft() {
         undraw()
         const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
@@ -126,7 +137,7 @@ document.addEventListener('DOMContentLoaded', () => {
         draw()
     }
 
-    //move the tetromino right, unless is at the edge or there is a blockage
+    // move the tetromino right, unless is at the edge or there is a blockage
     function moveRight() {
         undraw()
         const isAtRightEdge = current.some(index => (currentPosition + index + 1) % width === 0)
@@ -174,12 +185,12 @@ document.addEventListener('DOMContentLoaded', () => {
         draw()
     }
 
-    //show up-next tetromino in mini-grid display
+    // show up-next tetromino in mini-grid display
     const displaySquares = document.querySelectorAll('.mini-grid div')
     const displayWidth = 4
     const displayIndex = 0
   
-    //the Tetrominos without rotations
+    // the Tetrominos without rotations
     const upNextTetrominoes = [
       [1, displayWidth+1, displayWidth*2+1, 2], //lTetromino
       [0, displayWidth, displayWidth+1, displayWidth*2+1], //zTetromino
@@ -188,9 +199,9 @@ document.addEventListener('DOMContentLoaded', () => {
       [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] //iTetromino
     ]
 
-    //display the shape in the mini-grid display
+    // display the shape in the mini-grid display
     function displayShape() {
-        //remove any trace of a tetromino form the entire grid
+        // remove any trace of a tetromino form the entire grid
         displaySquares.forEach(square => {
             square.classList.remove('tetromino')
             square.style.backgroundColor = ''
@@ -198,23 +209,30 @@ document.addEventListener('DOMContentLoaded', () => {
         upNextTetrominoes[nextRandom].forEach( index => {
             displaySquares[displayIndex + index].classList.add('tetromino')
             displaySquares[displayIndex + index].style.backgroundColor = colors[nextRandom]
+            displaySquares[displayIndex + index].style.opacity = '0.9'
         })
     }    
 
-    //add functionality to the button
+    // add functionality to the button
     startBtn.addEventListener('click', () => {
-        if (timerId) {
-            clearInterval(timerId)
-            timerId = null
-        } else {
-            draw()
-            timerId = setInterval(moveDown, 1000)
-            nextRandom = Math.floor(Math.random()*theTetrominoes.length)
-            displayShape()
+        if (scoreDisplay.innerHTML !== 'end') {
+            startBtn.innerHTML = timerId ? 'START' : 'PAUSE'
+            if (isMusicPlay) {
+                playMusic()
+            }
+            if (timerId) {
+                clearInterval(timerId)
+                timerId = null
+            } else {
+                draw()
+                timerId = setInterval(moveDown, 1000)
+                nextRandom = Math.floor(Math.random()*theTetrominoes.length)
+                displayShape()
+            }
         }
     })
 
-    //add score
+    // add score
     function addScore() {
         for (let i = 0; i < 199; i +=width) {
             const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
@@ -227,6 +245,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 squares[index].classList.remove('tetromino')
                 squares[index].style.backgroundColor = ''
             })
+            playSound(sounds.clear)
             const squaresRemoved = squares.splice(i, width)
             squares = squaresRemoved.concat(squares)
             squares.forEach(cell => grid.appendChild(cell))
@@ -234,14 +253,42 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    //game over
+    // game over
     function gameOver() {
         if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
             scoreDisplay.innerHTML = 'end'
+            if(isMusicPlay) {
+                playMusic()
+            }
+            isMusicPlay = false
             clearInterval(timerId)
-        }
+            timerId = null
+        }  
     }
 
+    // Add main music
+    let mainMusic = document.getElementsByTagName("audio")[0]
+    function playMusic() {
+        if(mainMusic.paused) {
+            mainMusic.play()
+        } else {
+            mainMusic.pause()
+        }  
+    }
 
+    // Listener to music button
+    musicBtn.addEventListener('click', (mainMusic) => {
+        musicBtn.innerHTML = isMusicPlay ? String.fromCodePoint(0x1F508) : String.fromCodePoint(0x1F50A)
+        isMusicPlay = isMusicPlay ? false : true
+        if(timerId) {
+            playMusic()
+        }
+    })
+
+    function playSound(sound) {
+        let soundFall = document.getElementsByTagName("audio")[sound]
+        soundFall.loop = false
+        soundFall.play()
+    }
 })
 
